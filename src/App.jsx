481d40625dcf0f22939e0e8b45e58fd0d1837ed9
Bjorn Hagen <bjornhagen3@live.no>
2023-09-20 11:01:26 +0200

    renamed some folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/1. Header component/navbar'
-import Footer from './components/3. Footer comp/footer'
+import Footer from './components/3. Footer component/footer'
 import Projects from './components/2. Content comp/Projects component/projects'
 import About from './components/2. Content comp/About component/about'
 
