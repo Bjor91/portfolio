9f035e6169a51ec81aafd8a306f41fe54f3c3d6a
Bjorn Hagen <bjornhagen3@live.no>
2023-09-20 11:02:57 +0200

    corrected faulty import

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/1. Header component/navbar'
 import Footer from './components/3. Footer component/footer'
-import Projects from './components/2. Content comp/Projects component/projects'
-import About from './components/2. Content comp/About component/about'
+import Projects from './components/2. Content component/Projects component/projects'
+import About from './components/2. Content component/About component/about'
 
 const App = () =>  {
 
