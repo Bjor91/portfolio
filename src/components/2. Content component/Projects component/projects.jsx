a2af465771eb08c8b6b41f4014348c8d7cbf0150
Bjorn Hagen <bjornhagen3@live.no>
2023-09-27 11:07:01 +0200

    made gifs clickable

--- src/components/2. Content component/Projects component/projects.jsx
+++ src/components/2. Content component/Projects component/projects.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./projects.css";
-
+import ClickableGif from "../../4. Utility component/clickableGif";
 
 const Projects = () => {
+    
+    
     
     return (
         <div>
@@ -12,27 +14,30 @@ const Projects = () => {
 
 
         <div className = "project1">
-        <img src ="/gifs/1.gif" alt="project 1 gif"/>
+        <ClickableGif gifSrc="/gifs/3.gif" thumbnailSrc="/gifs/3.png" />
         <p> a gif showcasing project 1 coupled with a description a gif showcasing project 1 coupled with a description </p>
         </div>
       
         <div className = "project2">
-        <img src ="/gifs/2.gif" alt="project 2 gif"/>
         <p> a gif showcasing project 2 coupled with a description</p>
-        </div>
-        <div className = "project3">
+        <ClickableGif gifSrc="/gifs/3.gif" thumbnailSrc="/gifs/3.png" />
         
-        <img src ="/gifs/3.gif" alt="project 3 gif"/>
+        </div>
+
+        <div className = "project3">
+        <ClickableGif gifSrc="/gifs/3.gif" thumbnailSrc="/gifs/3.png" />
         <p> a gif showcasing project 3 coupled with a description</p>
         </div>
-        <div className = "project4">
         
-        <img src ="/gifs/4.gif" alt="project 4 gif"/>
+        
+        <div className = "project4">
         <p> a gif showcasing project 4 coupled with a description</p>
+        <ClickableGif gifSrc="/gifs/4.gif" thumbnailSrc="/gifs/3.png" />
+       
         </div>
         <div className = "project5">
         
-        <img src ="/gifs/5.gif" alt="project 5 gif"/>
+        <ClickableGif gifSrc="/gifs/5.gif" thumbnailSrc="/gifs/3.png" />
         <p> a gif showcasing project 5 coupled with a description</p>
         </div>
         </div>
---
+++ src/components/4. Utility component/clickableGif.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const ClickableGif = ({ gifSrc, thumbnailSrc }) => {
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    return (
+        <div onClick={() => setIsPlaying(!isPlaying)}
+        style={{ 
+            position: 'relative', 
+            cursor: 'pointer', 
+            }}
+          >
+            {!isPlaying && 
+        <div 
+          style={{ 
+            position: 'absolute', 
+            top: '50%', 
+            left: '50%', 
+            transform: 'translate(-50%, -50%)',
+            fontSize: '20em', 
+            color: 'white' 
+          }}
+        >
+          &#9654; {/* Unicode character for the play symbol */}
+        </div>
+      }
+      <img src={isPlaying ? gifSrc : thumbnailSrc} alt="project" />
+    </div>
+  );
+};
+
+export default ClickableGif;
