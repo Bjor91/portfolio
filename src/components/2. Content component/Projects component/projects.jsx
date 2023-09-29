f87b837b51168875aab37604291ff1496fd0b7cd
Bjorn Hagen <bjornhagen3@live.no>
2023-09-29 14:38:00 +0200

    maybe made it uglier

--- src/components/2. Content component/Projects component/projects.jsx
+++ src/components/2. Content component/Projects component/projects.jsx
@@ -8,42 +8,52 @@ const Projects = () => {
 
     return (
         <div>
-            <h1>Projects</h1>
+            <h1>{/*"Projects"*/}</h1>
 
             <div className="projects">
 
-
-                <div className="project1">
-                    
-                    <ClickableGif gifSrc="/gifs/1.gif" thumbnailSrc="/gifs/1.png" />
-                    <p> My contribution to a coding-challenge about saving. 
-                        This React application provides an interface to monitor and manage financial transactions and account details with dynamic time filtering and category management. 
-                    </p>  
+                <div className="project-Card1">
+                    <h2>SaveIt</h2>
+                    <div className="project1">
+                        <ClickableGif gifSrc="/gifs/1.gif" thumbnailSrc="/gifs/1.png" />
+                        <p> My contribution to a coding-challenge about saving. 
+                            This React application provides an interface to monitor and manage financial transactions and account details with dynamic time filtering and category management. 
+                        </p>  
+                    </div>
                 </div>
 
-                <div className="project2">
-                    <p> The backend of the SaveIt, set up using Java with the Spring Boot framework. Exposes a set of APIs that the web-app can call upon to retrieve data.</p>
-                    
-                    <ClickableGif gifSrc="/gifs/2.gif" thumbnailSrc="/gifs/2.png" />
-
+                <div className="project-Card2">
+                    <h2>Spring Boot Application</h2>
+                    <div className="project2">
+                        <p> The backend of the SaveIt, set up using Java with the Spring Boot framework. Exposes a set of APIs that the web-app can call upon to retrieve data.</p>
+                        <ClickableGif gifSrc="/gifs/2.gif" thumbnailSrc="/gifs/2.png" />
+                    </div>
                 </div>
 
-                <div className="project3">
-                    <ClickableGif gifSrc="/gifs/3.gif" thumbnailSrc="/gifs/3.png" />
-                    <p> Group project for a system-development course. Kill monsters, gain experience and use experience to upgrade ability. Fun game where your goal is to survive as long as possible, reaching the highest level.</p>
+                <div className="project-Card3">
+                    <h2>Southgame</h2>
+                    <div className="project3">
+                        <ClickableGif gifSrc="/gifs/3.gif" thumbnailSrc="/gifs/3.png" />
+                        <p> Group project for a system-development course. Kill monsters, gain experience and use experience to upgrade ability. Fun game where your goal is to survive as long as possible, reaching the highest level.</p>
+                    </div>
                 </div>
 
-                {/* 
-                <div className="project4">
-                    <p> a gif showcasing project 4 coupled with a description</p>
-                    <ClickableGif gifSrc="/gifs/4.gif" thumbnailSrc="/gifs/3.png" />
- 
-                </div>
-                <div className="project5">
+                {/*
+                <div className="project-Card4"> 
+                    <div className="project4">
+                        <p> a gif showcasing project 4 coupled with a description</p>
+                        <ClickableGif gifSrc="/gifs/4.gif" thumbnailSrc="/gifs/3.png" />
+    
+                    </div>
+                    </div>
+                <div className="project-Card5">
+                    <div className="project5">
 
-                    <ClickableGif gifSrc="/gifs/5.gif" thumbnailSrc="/gifs/3.png" />
-                    <p> a gif showcasing project 5 coupled with a description</p>
+                        <ClickableGif gifSrc="/gifs/5.gif" thumbnailSrc="/gifs/3.png" />
+                        <p> a gif showcasing project 5 coupled with a description</p>
+                    </div>
                 </div>*/}
+                
             </div>
         </div>
     );
